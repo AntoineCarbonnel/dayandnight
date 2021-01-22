a8ebd6b9b9d6a76b41b7ac1a22d1e965c88914a9
zerckel <antoinecarbonnel@yahoo.fr>
2021-01-22 18:01:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let http = require('http');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let sassMiddleware = require('node-sass-middleware');
+
+let indexRouter = require('./routes/index');
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.use((req, res,next)=>{
+  res.status(404).send('<h1> Page not found </h1>');
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
+module.exports = app;
